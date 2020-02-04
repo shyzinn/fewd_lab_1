a4e8cd345cb527e8bec96124e2691d9ee1ca57b5
shyzinn <gdesouza0688@gmail.com>
2020-02-04 11:43:01 +0000

    ‘commit’

---
+++ fewd_lab_1.js
@@ -0,0 +1,48 @@
+{/*JavaScript Lab 1
+Front-End Web Development
+
+Gustavo Henrique de Souza 
+ID: 20052 
+*/
+
+/*3. Declare a variable firstName and initialize it with the value of your name*/
+
+var firstName;
+    firstName = "Gustavo ";
+
+var favouriteColor;
+    favouriteColor = "Green";
+
+/*5. Several variables are declared and initialized in 2 program lines:*/
+
+var favouriteFood = 'Spanish Fabada';
+var city = 'Sao Paulo';
+
+/*6. Which value does x have after execution of the following code?*/
+
+x = 'Toe';
+
+/*7. Which value does y have after execution of the following code?*/
+
+y = x;
+
+/*8. Using console.log() display the value for your firstName, favouriteColor,
+favouriteFood and city variables as 3 separate statements.*/
+
+console.log("My name is : " + firstName + "and " + "my favourite color is: "+ favouriteColor);
+console.log("My favourite food is: " + favouriteFood);
+console.log("My city  is: " + city);
+
+var phrase = 'A rainbow is formed by the sunlight. Shining through rain drops so small. The rain bends the light from the sunshine, and colors the sky for us all';
+var endingPhrase = 'is what we see. Rainbow colours, are made with light bending you see!';
+var one = 'Red';
+var two = 'orange';
+var three = 'yellow';
+var four = 'green';
+var five = 'blue';
+var six = 'indigo';
+var seven = 'violet';
+
+console.log(phrase + ". " + one + ", " + two + ", " + three + ", " + four + ", " + five + ", " + six + ", " + seven + " " + endingPhrase);
+
+
